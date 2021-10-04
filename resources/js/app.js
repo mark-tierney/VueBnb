67bacc24f29e954779461a59c70958d66b537e3a
mark-tierney <marktierney66@gmail.com>
2021-10-04 11:46:16 -0700

    review resource and star rating component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ require("./bootstrap");
 import VueRouter from "vue-router";
 import router from "./routes";
 import Index from "./Index";
+import moment from "moment";
+import StarRating from "./shared/components/StarRating";
 
 window.Vue = require("vue").default;
 // Vue.component(
@@ -14,6 +16,9 @@ window.Vue = require("vue").default;
 // );
 
 Vue.use(VueRouter);
+Vue.filter("fromNow", value => moment(value).fromNow());
+
+Vue.component("star-rating", StarRating);
 
 const app = new Vue({
     el: "#app",
--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -41,6 +41,9 @@
 
 <script>
 export default {
+    props:{
+        bookableId: String
+    },
     data(){
         return{
             from: null,
@@ -54,7 +57,7 @@ export default {
         check(){
             this.errors = null;
             this.loading = true;
-            axios.get(`/api/bookables/${this.$route.params.id}/availability?from=${this.from}&to=${this.to}`
+            axios.get(`/api/bookables/${this.bookableId}/availability?from=${this.from}&to=${this.to}`
             ).then(response => {
                 this.status = response.status;
             }).catch(error => {
