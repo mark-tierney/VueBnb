67655feed84db1d7cc0cec8751fa8adf06f31b3c
mark-tierney <marktierney66@gmail.com>
2021-10-09 13:38:21 -0700

    booking-by-review resources and form styles

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,24 +1,37 @@
 <template>
-    <div>
-        <div v-if="loading">
-            Loading...
-        </div>
-        <div v-else>
-            <div v-if="alreadyReviewed">
-                <h3>You've already reviewed this booking.</h3>
+
+    <div class="row">
+        <div :class="[{'col-md-4': loading || !alreadyReviewed}, {'d-none': !loading && alreadyReviewed}]">
+            <div class="card">
+                <div class="card-body">
+                    <div v-if="loading">Loading...</div>
+                    <div v-else>
+                        <p>Stayed at <router-link :to="{name: 'bookable', params: { id: booking.bookable.bookable_id }}">{{booking.bookable.title}}</router-link></p>
+                        <p>From {{ booking.from }} to {{booking.to}}</p>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div :class="[{'col-md-8': loading || !alreadyReviewed}, {'col-md-12': !loading && alreadyReviewed}]">
+            <div v-if="loading">Loading...</div>
             <div v-else>
-                <div class="form-group">
-                    <label for="" class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
-                    <star-rating class="fa-2x" v-model="review.rating"></star-rating>
+                <div v-if="alreadyReviewed">
+                    <h3>You've already reviewed this booking.</h3>
                 </div>
-                <div class="form-group">
-                    <label for="content" class="text-muted">Describe your experience</label>
-                    <textarea name="content" class="form-control" cols="30" rows="10" v-model="review.content"></textarea>
+                <div v-else>
+                    <div class="form-group">
+                        <label for="" class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
+                        <star-rating class="fa-2x" v-model="review.rating"></star-rating>
+                    </div>
+                    <div class="form-group">
+                        <label for="content" class="text-muted">Describe your experience</label>
+                        <textarea name="content" class="form-control" cols="30" rows="10" v-model="review.content"></textarea>
+                    </div>
+                    <button class="btn btn-lg btn-primary btn-block">Submit</button>
                 </div>
-                <button class="btn btn-lg btn-primary btn-block">Submit</button>
             </div>
         </div>
+
     </div>
 </template>
 
@@ -31,20 +44,35 @@ export default {
                 content: null
             },
             existingReview: null,
-            loading: false
+            loading: false,
+            booking: null
         };
     },
     created(){
         this.loading = true;
-        axios
+        axios //if review already exists (in reviews table by id)
             .get(`/api/reviews/${this.$route.params.id}`)
             .then(response =>(this.existingReview = response.data.data))
-            .catch(err => {})
+            .catch(err => {
+                if(err.response && err.response.status && 404 == err.response.status){
+                    return axios //fetch booking by a review key
+                        .get(`/api/booking-by-review/${this.$route.params.id}`)
+                        .then(response => {
+                            this.booking = response.data.data;
+                        });
+                }
+            })
             .then(() =>(this.loading = false));
     },
     computed: {
         alreadyReviewed(){
+            return this.hadReview || !this.booking;
+        },
+        hasReview(){
             return this.existingReview != null;
+        },
+        hasBooking(){
+            return this.booking != null;
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@
 use App\Http\Controllers\Api\BookableAvailabilityController;
 use App\Http\Controllers\Api\BookableController;
 use App\Http\Controllers\Api\BookableReviewController;
+use App\Http\Controllers\Api\BookingByReviewController;
 use App\Http\Controllers\Api\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -38,4 +39,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('bookables', BookableController::class)->only(['index', 'show']);
 Route::get('bookables/{bookable}/availability', BookableAvailabilityController::class)->name('bookables.availability.show');
 Route::get('bookables/{bookable}/reviews', BookableReviewController::class)->name('bookables.reviews.index');
+Route::get('/booking-by-review/{reviewKey}', BookingByReviewController::class)->name('booking.by-review.show');
 Route::apiResource('reviews', ReviewController::class)->only(['show']);
+
